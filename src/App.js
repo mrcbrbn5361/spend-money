d361c5dd4ab7c8f3144bc5d6957d22842fda47da
adilett02 <adiletturdakunov2002@gmail.com>
2021-08-27 12:41:48 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Item } from './components/Item';
+
+function App() {
+  const [money, setMoney] = useState(1000000);
+  const [data, setData] = useState([]);
+
+  // let changeMoney = (greens) => {
+  //   setMoney(money - greens);
+  // };
+
+  // let changeMoney2 = (greens) => {
+  //   setMoney(money + greens);
+  // };
+
+  // let changeMoney3 = (greens) => {
+  //   setMoney(money - greens);
+  // };
+
+  let updateMoney = (greens) => {
+    setMoney(money - greens);
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data.items);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <img src="https://neal.fun/spend/billgates.jpg" alt="" />
+        <h1>Spend Bill Gates' Money</h1>
+      </header>
+      <div className="money">${money}</div>
+      <main>
+        <div className="container">
+          <div className="items__list">
+            {data.map((data) => (
+              <Item
+                data={data}
+                changeMoney={changeMoney}
+                changeMoney2={changeMoney2}
+                changeMoney3={changeMoney3}
+                updateMoney={updateMoney}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+export const Item = ({ data, changeMoney, changeMoney2, changeMoney3, updateMoney }) => {
+  const [counter, setCounter] = useState(0);
+  let buyClick = () => {
+    setCounter(counter + 1);
+    // changeMoney(data.price);
+  };
+  let sellClick = () => {
+    setCounter(counter - 1);
+    // changeMoney2(data.price);
+  };
+
+  let onHandleChange = (event) => {
+    console.log('input changed');
+    console.log(event.target.value);
+    setCounter(event.target.value);
+    console.log(counter);
+    // changeMoney3(event.target.value * data.price);
+  };
+
+  useEffect(() => {
+    updateMoney(counter * data.price);
+  }, [counter]);
+  return (
+    <div className="item">
+      <img src={data.image} alt="" />
+      <div className="item__name">{data.name}</div>
+      <div className="item__price">${data.price}</div>
+      <div className="counter">
+        <button disabled={counter === 0 ? true : false} onClick={sellClick}>
+          Sell
+        </button>
+        <input type="number" value={counter} onChange={onHandleChange} />
+        <button onClick={buyClick}>Buy</button>
+      </div>
+    </div>
+  );
+};
