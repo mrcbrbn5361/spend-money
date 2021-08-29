5184231dd5f91251947cc4d3bf6efa37646194de
Adilet <adiletturdakunov2002@gmail.com>
2021-08-29 17:30:36 +0600

    .

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Input } from './components/Input';
 import { Item } from './components/Item';
 import obj from './myStore';
 
-const money = 100000000000;
+const money = 100_000_000_000;
 
 function App() {
+  console.log((123456789).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
   const [info, setInfo] = useState({});
-  console.log(info);
 
   const [items, setItems] = useState([]);
 
@@ -17,23 +17,6 @@ function App() {
   }, []);
   return (
     <div className="App">
-      {/* <div>
-        Money:{''}
-        {money - Object.values(info).reduce((res, cur) => res + cur.count * cur.price, 0)}
-      </div>
-
-      <div>
-        {items.map((item) => (
-          <Input
-            key={item.id}
-            onChange={setInfo}
-            price={item.price}
-            itemKey={item.name}
-            title={item.name}
-          />
-        ))}
-      </div> */}
-
       <header>
         <div className="container">
           <div width="1000" className="header">
@@ -48,105 +31,13 @@ function App() {
           Spend Bill Gates' Money
         </h1>
         <div className="countzz">
-          $100,000,000,000
+          ${(money - Object.values(info).reduce((res, cur) => res + cur.count * cur.price, 0)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </div>
         <div className="row mt-2 px-2">
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-sm-6 col-12 px-1 my-2">
-            <div className="product bg-white">
-              <img className="ml-auto mr-auto" src="https://neal.fun/spend/images/big-mac.jpg" alt="" />
-              <h3>Бургер</h3>
-              <p className="product__price"><span className="symbol">$</span>2<span className="price"></span></p>
-              <div className="buy-sell d-flex justify-content-between">
-                <button className="sell">Продать</button>
-                <input className="quantity" type="number" value="0" />
-                <button className="buy">Купить</button>
-              </div>
-            </div>
-          </div>
+          {items.map((item) => (
+            <Item key={item.id} onChange={setInfo} itemKey={item.name} base={item} price={item.price}
+              title={item.name} />
+          ))}
         </div>
 
       </div>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,38 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
-export const Item = ({ data, changeMoney, changeMoney2, changeMoney3, updateMoney }) => {
-  const [counter, setCounter] = useState(0);
-  let buyClick = () => {
-    setCounter(counter + 1);
-    // changeMoney(data.price);
-  };
+export const Item = ({ base, onChange, price, }) => {
+
+  let [count, setCount] = useState(0);
+
   let sellClick = () => {
-    setCounter(counter - 1);
-    // changeMoney2(data.price);
+    setCount(count - 1);
+  };
+  let buyClick = () => {
+    setCount(Number(count) + 1);
   };
 
-  let onHandleChange = (event) => {
-    // console.log('input changed');
-    // console.log(event.target.value);
-    setCounter(event.target.value);
-    // console.log(counter);
-    // changeMoney3(event.target.value * data.price);
+  let changed = (e) => {
+    setCount((e.target.value).replace(/^0+/, ''))
   };
-
   useEffect(() => {
-    updateMoney(counter * data.price);
-  }, [counter]);
+    onChange((prev) => {
+      return {
+        ...prev,
+        [base.name]: {
+          price, count
+        },
+      };
+    });
+  }, [count]);
   return (
-    <div className="item">
-      <img src={data.image} alt="" />
-      <div className="item__name">{data.name}</div>
-      <div className="item__price">${data.price}</div>
-      <div className="counter">
-        <button disabled={counter === 0 ? true : false} onClick={sellClick}>
-          Sell
-        </button>
-        <input type="number" value={counter} onChange={onHandleChange} />
-        <button onClick={buyClick}>Buy</button>
+    <div className="col-lg-4 col-sm-6 col-12 px-1 my-1">
+      <div className="product bg-white">
+        <img className="ml-auto mr-auto" src={base.image} alt="" />
+        <h3>{base.name}</h3>
+        <p className="product__price">
+          <span className="symbol">$</span>
+          <span className="price">{(base.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></p>
+        <div className="buy-sell d-flex justify-content-between">
+          <button disabled={count < 1 ? true : false} onClick={sellClick} className={`sell ${count < 1 ? 'disabled' : ''}`}>Sell</button>
+          <input className="quantity" value={count || 0} onChange={changed} type="number" />
+          <button onClick={buyClick} className="buy">Buy</button>
+        </div>
       </div>
     </div>
   );
